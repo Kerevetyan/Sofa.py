8efc6c160ce7555a40553b138d78ea3ce43ea6db
Kerevetyan <sophiasophia8486@gmail.com>
2025-04-24 15:38:07 +0200

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,26 +1,62 @@
 import sqlite3
-conn = sqlite3.connect('my_database3.db')
-cursor = conn.cursor()
 
-# Створення таблиці (якщо її ще немає)
+conn = sqlite3.connect("my_database.db")
+cursor = conn.cursor()
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS library (
+CREATE TABLE IF NOT EXISTS menu (
 
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-author TEXT,
-song TEXT,
-game TEXT,
-comics TEXT,
-year INTEGER
+dish TEXT,
+category TEXT,
+price INT,
+available TEXT
 )
 ''')
+cursor.execute("INSERT INTO menu (dish, category, price, available) VALUES ('chiken', 'main_dish', 66, 'YES')")
+cursor.execute("INSERT INTO menu (dish, category, price, available) VALUES ('reebs', 'main_dish', '85', 'YES')")
+cursor.execute("INSERT INTO menu (dish, category, price, available) VALUES ('cake', 'desert', '25', 'YES')")
+cursor.execute("INSERT INTO menu (dish, category, price, available) VALUES ('cheese_cake', 'desert', '30', 'NO')")
+cursor.execute("INSERT INTO menu (dish, category, price, available) VALUES ('steak', 'main_dish', '30', 'NO')")
+cursor.execute("INSERT INTO menu (dish, category, price, available) VALUES ('ice_cream', 'desert', '40', 'YES')")
 
-cursor.execute("INSERT INTO library (song, author, year) VALUES ('heartbeat', 'childish gambino', 2012)")
-cursor.execute("INSERT INTO library (game, author, year) VALUES ('S. T. A. L. K. E. R. 2', 'Yaroslav Kravchenko', 2024)")
-cursor.execute("INSERT INTO library (comics, author, year) VALUES ('Jujutsu Kaisen', 'Gege Akutami', 2018)")
+cursor.execute("SELECT dish, category, price, available FROM menu WHERE dish LIKE '%reebs%'")
+print(cursor.fetchall())
+cursor.execute("SELECT dish, category, price, available FROM menu WHERE category LIKE '%desert%'")
+print(cursor.fetchall())
+cursor.execute("SELECT dish, category, price, available FROM menu WHERE price < 30 AND available LIKE '%YES%'")
+print(cursor.fetchall())
+cursor.execute("SELECT dish, category, price, available FROM menu WHERE price > 50 AND category LIKE '%main_dish%'")
+print(cursor.fetchall())
 
-cursor.execute("SELECT * FROM library")
-print(cursor.fetchall()) # Вивести всі записи з таблиці
+conn.commit()
+conn.close()
 
-# Закриття з'єднання
-conn.close() 
+
+
+import sqlite3
+
+conn = sqlite3.connect("my_database.db")
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS music (
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+song TEXT,
+author TEXT,
+year INT
+)
+''')
+cursor.execute("INSERT INTO music (song, author, year) VALUES ('Born To Die', 'Lana Del Rey', 2012)")
+cursor.execute("INSERT INTO music (song, author, year) VALUES ('heartbeat', 'childish gambino', 2012)")
+cursor.execute("INSERT INTO music (song, author, year) VALUES ('Pretty', 'Coco & Clair Clair', 2021)")
+cursor.execute("INSERT INTO music (song, author, year) VALUES ('Renegade', 'Aaryan Shah', 2022)")
+
+cursor.execute("SELECT song, author, year FROM music WHERE song LIKE '%B%'")
+print(cursor.fetchall())
+cursor.execute("SELECT song, author, year FROM music WHERE author LIKE '%C%'")
+print(cursor.fetchall())
+cursor.execute("SELECT song, author, year FROM music WHERE year > 2013")
+print(cursor.fetchall())
+
+conn.commit()
+conn.close()
